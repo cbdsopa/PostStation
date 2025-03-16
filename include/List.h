7b91942c0bd3164b541f976f85835cea1e2b1dfe
cbdsopa <1425557647@qq.com>
2025-03-16 13:24:24 +0800

    ListPro

--- include/List.h
+++ include/List.h
@@ -14,3 +14,10 @@ typedef struct{
 int List_Init(List *);
 int List_Insert(List *, const void *, const size_t);
 int List_Delete(List *, const void *, const size_t);
+int List_Find(List *, const void *, const size_t);
+int List_DeleteTheEarliest(List *);
+int List_DeleteTheLatest(List *);
+void *List_ShowTheEarliest(List *, int);
+	// 返回数据指针，强制转换为对应类型后使用
+void *List_ShowTheLatest(List *, int);
+	// 返回数据指针，强制转换为对应类型后使用
--- src/List.c
+++ src/List.c
@@ -7,6 +7,7 @@ int List_Init(List *list){
 	list->count = 0;
 	return 0;
 }
+
 int List_Insert(List *list, const void *data, const size_t siz){
 	if(list == NULL) return -1;
 	if(data == NULL) return -2;
@@ -23,6 +24,7 @@ int List_Insert(List *list, const void *data, const size_t siz){
 	++list->count;
 	return 0;
 }
+
 int List_Delete(List *list, const void *data, const size_t siz){
 	if(list == NULL) return -1;
 	if(data == NULL) return -2;
@@ -45,4 +47,68 @@ int List_Delete(List *list, const void *data, const size_t siz){
 		}
 	}
 	return 0;
+}
+
+int List_Find(List *list, const void *data, const size_t siz){
+	if(list == NULL) return -1;
+	if(data == NULL) return -2;
+	ListNode *now = list->head;
+	while(now != NULL){
+		if(memcmp(now->data, data, siz) == 0){
+			return 1;
+		}
+		now = now->next;
+	}
+	return 0;
+}
+
+int List_DeleteTheEarliest(List *list){
+	if(list == NULL) return -1;
+	if(list->count < 1) return 0;
+	ListNode *now = list->head;
+	while(now->next != NULL){
+		now = now->next;
+	}
+	free(now->next);
+	now->next = NULL;
+	return 1;
+}
+
+int List_DeleteTheLatest(List *list){
+	if(list == NULL) return -1;
+	if(list->count < 1) return 0;
+	ListNode *temp = list->head;
+	free(temp);
+	list->head = list->head->next;
+	return 1;
+}
+
+void *List_ShowTheEarliest(List *list, int index){
+	if(list == NULL) return NULL;
+	if(list->count < index || index < 1) return NULL;
+	ListNode *now = list->head;
+	int count = 0;
+	while(now != NULL){
+		if(list->count - count == index){
+			return now->data;
+		}
+		++count;
+		now = now->next;
+	}
+	return NULL;
+}
+
+void *List_ShowTheLatest(List *list, int index){
+	if(list == NULL) return NULL;
+	if(list->count < index || index < 1) return NULL;
+	ListNode *now = list->head;
+	int count = 0;
+	while(now != NULL){
+		++count;
+		if(count == index){
+			return now->data;
+		}
+		now = now->next;
+	}
+	return NULL;
 }
